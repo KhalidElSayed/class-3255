477805bee481cd19d99ca0c0dd10dd06a843e4ce
Aleksandar Gargenta <sasa@gargenta.com>
2013-07-15 14:11:40 -0700

    Posting tweets to log

--- Yamba/src/com/twitter/android/yamba/StatusActivity.java
+++ Yamba/src/com/twitter/android/yamba/StatusActivity.java
@@ -1,17 +1,33 @@
+
 package com.twitter.android.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+
+    private static final String TAG = StatusActivity.class.getSimpleName();
+
+    private Button statusButton;
+
+    private EditText statusText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-    }
 
+        this.statusButton = (Button) super.findViewById(R.id.status_button);
+        this.statusText = (EditText) super.findViewById(R.id.status_text);
+
+        this.statusButton.setOnClickListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +35,10 @@ public class StatusActivity extends Activity {
         getMenuInflater().inflate(R.menu.status, menu);
         return true;
     }
-    
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, this.statusText.getText().toString());
+    }
+
 }
