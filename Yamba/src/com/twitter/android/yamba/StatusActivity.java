c13584cdc4201ac1f27d592d5a8a2197768db876
Aleksandar Gargenta <sasa@gargenta.com>
2013-07-15 15:29:04 -0700

    Added text counter with externalized colors

--- Yamba/src/com/twitter/android/yamba/StatusActivity.java
+++ Yamba/src/com/twitter/android/yamba/StatusActivity.java
@@ -3,20 +3,33 @@ package com.twitter.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher {
 
     private static final String TAG = StatusActivity.class.getSimpleName();
 
     private Button statusButton;
 
     private EditText statusText;
+
+    private TextView statusCounter;
+
+    private int maxChars;
+
+    private int statusCounterDefaultColor;
+
+    private int statusCounterWarningColor;
+
+    private int statusCounterErrorColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +38,17 @@ public class StatusActivity extends Activity implements OnClickListener {
 
         this.statusButton = (Button) super.findViewById(R.id.status_button);
         this.statusText = (EditText) super.findViewById(R.id.status_text);
+        this.statusCounter = (TextView) super.findViewById(R.id.status_counter);
+        this.maxChars = Integer.parseInt(super.getText(R.string.max_chars).toString());
+        this.statusCounter.setText(R.string.max_chars);
+        this.statusCounterDefaultColor = this.statusCounter.getCurrentTextColor();
 
         this.statusButton.setOnClickListener(this);
+        this.statusText.addTextChangedListener(this);
+
+        this.statusCounterWarningColor = super.getResources().getColor(
+                R.color.status_counter_warning);
+        this.statusCounterErrorColor = super.getResources().getColor(R.color.status_counter_error);
     }
 
     @Override
@@ -39,6 +61,27 @@ public class StatusActivity extends Activity implements OnClickListener {
     @Override
     public void onClick(View view) {
         Log.d(TAG, this.statusText.getText().toString());
+    }
+
+    @Override
+    public void afterTextChanged(Editable arg0) {
+        int length = this.maxChars - this.statusText.length();
+        if (length < 0) {
+            this.statusCounter.setTextColor(this.statusCounterErrorColor);
+        } else if (length < 10) {
+            this.statusCounter.setTextColor(this.statusCounterWarningColor);
+        } else {
+            this.statusCounter.setTextColor(this.statusCounterDefaultColor);
+        }
+        this.statusCounter.setText(String.valueOf(length));
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
     }
 
 }
